ae9ba71f8bbcc59762f482f2b77aea2ed22bd1fc
tomasz.wasowicz@gmail.com <tomasz.wasowicz@gmail.com>
2025-03-29 10:33:53 +0100

    29.03.2025 - setup and loop functions

--- esp32_ble_client.cpp
+++ esp32_ble_client.cpp
@@ -170,3 +170,45 @@ void setup() {
     display.setCursor(0, 25);
     display.print("BLE Client");
     display.display();
+
+    //Start serial communication
+    Serial.begin(115200);
+    Serial.println("Starting Arduino BLE Client application...");
+
+    //Init BLE device
+    BLEDevice::init("");
+
+    // Retrieve a Scanner and set the callback we want to use to be informed when we
+    // have detected a new device.  Specify that we want active scanning and start the
+    // scan to run for 30 seconds.
+    BLEScan* pBLEScan = BLEDevice::getScan();
+    pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+    pBLEScan->setActiveScan(true);
+    pBLEScan->start(30);
+}
+
+void loop() {
+    // If the flag "doConnect" is true then we have scanned for and found the desired
+    // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
+    // connected we set the connected flag to be true.
+    if (doConnect == true) {
+        if (connectToServer(*pServerAddress)) {
+            Serial.println("We are now connected to the BLE Server.");
+            //Activate the Notify property of each Characteristic
+            temperatureCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
+            humidityCharacteristic->getDescriptor(BLEUUID((uint16_t)0x2902))->writeValue((uint8_t*)notificationOn, 2, true);
+            connected = true;
+        }
+        else {
+            Serial.println("We have failed to connect to the server; Restart your device to scan for nearby BLE server again.");
+        }
+        doConnect = false;
+    }
+    //if new temperature readings are available, print in the OLED
+    if (newTemperature && newHumidity) {
+        newTemperature = false;
+        newHumidity = false;
+        printReadings();
+    }
+    delay(1000); // Delay a second between loops.
+}
